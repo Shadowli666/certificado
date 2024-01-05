bd2b8028e33ca27c1cb3199a0abd6f388052a7e2
Luis Felipe <shadowli666@gmail.com>
2024-01-05 15:53:11 -0400

    fix: typo in facultad migration name

--- database/migrations/2024_01_04_090345_create_facultad_table.php
+++ database/migrations/2024_01_04_090345_create_facultad_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('facultads', function (Blueprint $table) {
+        Schema::create('facultad', function (Blueprint $table) {
             $table->id();
-            $table->string('faculty', 30)->default('Default Faculty Name');
+            $table->string('name', 30)->default('Default Faculty Name');
             $table->timestamps();
         });
     }
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('facultads');
+        Schema::dropIfExists('facultad');
     }
 };
