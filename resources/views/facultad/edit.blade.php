d8f1878af4eb6b1579a72f4664e34506853e4984
Luis Felipe <shadowli666@gmail.com>
2024-01-07 11:01:28 -0400

    Typo: Fix typo in facultad.edit view

--- resources/views/facultad/edit.blade.php
+++ resources/views/facultad/edit.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="container-fluid min-vh-100">
         <div class="text-center">
-            <h1>SISEAD - REGISTRO DE PERIODO</h1>
+            <h1>Edicion de Facultad</h1>
         </div>
         <div class="form-container container my-5">
         <form action="{{url('/facultad/'.$facultad->id)}}" method="post" class="form-container container my-5">
