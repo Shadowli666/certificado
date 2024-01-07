cbc4c222a790f1d5f3b34690490f46b54c0a01c3
Luis Felipe <shadowli666@gmail.com>
2024-01-07 06:11:08 -0400

    Feat: Navbar certificado item

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,9 @@
                         <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="{{route('facultad.index')}}">Facultad</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="{{route('certificado.index')}}">Certificado</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
